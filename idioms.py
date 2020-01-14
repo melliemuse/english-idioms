35a714232c342e54b356c3a8688dc2036cfe94c4
Melody Stern <melliemuse@gmail.com>
2020-01-13 21:11:58 -0600

    completed exercise

---
+++ idioms.py
@@ -0,0 +1,27 @@
+
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"], 
+}
+# joining the strings
+for items in idioms.items():
+    print(f"{items[0]}: {' '.join(items[1])}")
+# Output
+# Penny: A penny for your thoughts
+# Injury: Add insult to injury
+# Moon: Once in a blue moon
+# Grape: I heard it through the grapevine
+# Murder: Kill two birds with one stone
+# Limbs: It costs an arm and a leg
+# Grain: Take what someone says with a grain of salt
+# Fences: I'm on the fence about it
+# Sheep: Pulled the wool over his eyes
+# Lucifer: Speak of the devil
